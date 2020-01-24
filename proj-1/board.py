9b7c19743bdbc9738fbc8d59d2a8b1ba8107d62e
s-kyy <samantha.y.twentyfourteen@gmail.com>
2020-01-24 00:40:35 -0500

    Initialized Project1.1 files: board and dfs

---
+++ proj-1/board.py
@@ -0,0 +1,26 @@
+""" 
+Project 1.1: Indonesian Dot Puzzle Board Class
+COMP 472 NN 
+DUE: Feb 9th, 2020
+Samantha Yuen (40033121), Andrew Marcos (40011252), Michael Gagnon (40030481)
+
+Variables include: max_d (depth), max_l (search path length)
+Constructors that takes in input file and creates objects of varying board sizes 3x3, 4x4 and 5x5 grids
+Methods to modify tokens on the board adjacent to input coordinates 
+Method to return if game is finished (all 0s)
+Method to return number of 1s on the board (for heuristic function?)
+Method to return the coordinates of 1s on the board (heursitic function)
+"""
+class Board:
+
+    """ Variables """
+    int max_d # maximum depth used in iterative depth-first search
+    #int max_l # maximum search path length in breadth-first search + Alg.A*
+    
+    """Constructors"""
+    def __init__():
+        
+
+    """Methods"""
+
+
---
+++ proj-1/dfs.py
@@ -0,0 +1,12 @@
+"""
+Project 1.1: Depth First Search Algorithm
+COMP 472 NN 
+DUE: Feb 9th, 2020
+Samantha Yuen (40033121), Andrew Marcos (40011252), Michael Gagnon (40030481)
+
+Open list = stack
+Tree Data structure -- From the root/parent, we can have nxn different children 
+depth-level should start at 0 or 1
+Must output dfs.solution.txt and dfs.search.txt files 
+"""
+
