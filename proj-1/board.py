9ebef2ce96fe3b42eaa6005a342325f127f86417
s-kyy <samantha.y.twentyfourteen@gmail.com>
2020-02-01 15:14:44 -0500

    Board class Constructor complete

--- proj-1/board.py
+++ proj-1/board.py
@@ -11,16 +11,40 @@ Method to return if game is finished (all 0s)
 Method to return number of 1s on the board (for heuristic function?)
 Method to return the coordinates of 1s on the board (heursitic function)
 """
-class Board:
+import numpy as np
 
-    """ Variables """
-    int max_d # maximum depth used in iterative depth-first search
-    #int max_l # maximum search path length in breadth-first search + Alg.A*
+class Board:
     
     """Constructors"""
-    def __init__():
-        
+    def __init__(self, num, maxD, size, state):
+        self.number = num   # index of puzzle
+        self.maxD = maxD    # maximum depth (for Depth-First Search)
+        self.size = size    # Size of the puzzle (3-10)
+        if (isinstance(state, str)):
+            state_splitted = [state[i] for i in range(0,len(state), 1)] 
+                            # Split the content of the board into an array of 1s and 0s
+        self.state = np.array(state_splitted).reshape(size, size)
+                            # Store the board state as a size by size ndarray
+
 
     """Methods"""
+    def touch(coord): 
+        pass
 
+    # def visualize():
+    #     pass
 
+    def toString(self):
+        #EX: 0 0 0 101010101
+        result = self.state
+        return result
+
+    # def is_equal(self, Board):
+    #     pass
+
+    def convertCoord(coord):
+        # switch = {
+        #     'A': 0
+        #     'B': 
+        # }
+        pass
