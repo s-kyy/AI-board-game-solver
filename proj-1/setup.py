e6b74690200b97e21dcbfea75fe759e09f0936c9
s-kyy <samantha.y.twentyfourteen@gmail.com>
2020-02-02 15:18:49 -0500

    atGoal function added

--- proj-1/setup.py
+++ proj-1/setup.py
@@ -1,4 +1,19 @@
+""" 
+Project 1.1: Indonesian Dot Puzzle Board Class
+COMP 472 NN 
+DUE: Feb 9th, 2020
+Samantha Yuen (40033121), Andrew Marcos (40011252), Michael Gagnon (40030481)
 
+Purpose:
+IO setup file to import the indonesian puzzle input.txt file and create a list of Boards.
+
+Methods:
+  createBoards(input) -- takes a string containing the input file name, outputs a list of Board puzzle (1 Board line)
+  atGoal(Board) -- takes a Board state as input and outputs a boolean. True if all values are 0, False otherwise
+  
+PUT IN DFS:
+  sortChildren(Boards) -- takes a list of board states and reorders them according to the position of zeroes. 
+"""
 
 num = 0
 
@@ -16,9 +31,9 @@ with open('test.txt','r') as f:
   
 
 
-  
-  
-
-    
-
-    
+  def atGoal(state):
+    for row in state:   # loop through each value in the board's state (2D array)
+      for val in row:
+        if val != 0:
+          return False  # if there is a single 1 in the matrix return False
+    return True         # otherwise return True
